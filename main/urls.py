e094f9eddd1211a16dbddc87fc01a2490d948b20
kimsehan11 <ggg09257@naver.com>
2025-12-01 11:22:16 +0900

    fix: add urlpatterns for signup page

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,7 @@ app_name = 'main'
 urlpatterns = [
     path('',views.main_view, name='main'),
     path('signup/',views.signup,name='signup'),
+    path('signup/form/',views.signup_form,name='signup_form'),
     path('find_password/',views.find_password,name='find_password'),
 ]
 
