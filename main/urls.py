67011b6a4e05290be400907d94b62690dcedfdf2
ssshinnpson <ddae0000@gmail.com>
2025-12-02 12:29:16 +0900

    fix: modify gallery url name

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,6 @@ app_name = 'main'
 
 urlpatterns = [
     path('',views.main_view, name='main'),
-    path('gallery/',views.gallery, name='main'),
+    path('gallery/',views.gallery, name='gallery'),
 ]
 
