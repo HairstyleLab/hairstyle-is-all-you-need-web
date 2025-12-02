7ee299aa6a019b7f934efe39e3c80dfce60662f4
kimsehan11 <ggg09257@naver.com>
2025-12-02 11:24:52 +0900

    feat: add change-password URL patterns to pass current and new passwords to views

--- uauth/urls.py
+++ uauth/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('send-password-reset-code/', views.send_password_reset_code, name='send_password_reset_code'),
     path('verify-code/', views.verify_code, name='verify_code'),
     path('reset-password/', views.reset_password, name='reset_password'),
+    path('change-password/', views.change_password, name='change_password'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('signup/', views.signup, name='signup'),
