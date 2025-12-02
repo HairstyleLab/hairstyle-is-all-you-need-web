4dcda9dc4c21ad8e8d3b52a580eeaf0b86b82426
kimsehan11 <ggg09257@naver.com>
2025-12-02 11:11:40 +0900

    feat: connect mysql to django

--- hairstyleLab/settings.py
+++ hairstyleLab/settings.py
@@ -83,25 +83,26 @@ WSGI_APPLICATION = "hairstyleLab.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': DB_ENGINE,
-#         'NAME': DB_NAME,
-#         'USER': DB_USER,
-#         'PASSWORD': DB_PASSWORD1,
-#         'HOST': DB_HOST,
-#         'PORT': DB_PORT,
-#         'OPTIONS': {
-#             'charset': 'utf8mb4'
-#         }
-#     }
-# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': DB_ENGINE,
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD1,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT,
+        'OPTIONS': {
+            'charset': 'utf8mb4'
+        }
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
